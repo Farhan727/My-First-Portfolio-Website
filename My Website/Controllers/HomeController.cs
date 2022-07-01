5b9aab46e991fd16fb9b6eff339118dbd2ed2769
Farhan Web Developer <89845305+Farhan727@users.noreply.github.com>
2022-07-01 12:14:52 +0500

    This is My Portfolio Website
    This is My Portfolio Website including technologies like
     HTML
    CSS
    JavaScript

---
+++ My Website/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_Website.Models;
+using System.Diagnostics;
+
+namespace My_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+        public IActionResult Services()
+        {
+            return View();
+            //we can also write this return *** View("Services"); *** same result
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
